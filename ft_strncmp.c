5fd05485f50b7177f37e4870210d1e8c9d514b36
mgomes-d <104273811+mgomes-d@users.noreply.github.com>
2023-08-11 08:17:24 +0200

    Fix ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,18 +14,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
-	if (!s1 || !s2)
-		return (0);
-	if (n == 0)
+	str1 = (unsigned char *) s1;
+	str2 = (unsigned char *) s2;
+	if (n < 1)
 		return (0);
 	i = 0;
-	while (i < n && (s1[i] || s2[i]))
-	{
-		if (s1[i] != s2[i])
-			return ((unsigned char)s1[i] - s2[i]);
+	while (str1[i] && str1[i] == str2[i] && i < n - 1)
 		i++;
-	}
-	return (0);
+	return (str1[i] - str2[i]);
 }
